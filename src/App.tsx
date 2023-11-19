7baf1dcc53d69e4b059a5dfb9570b9a28a3f079a
jayasuryas06@gmail.com <jayasuryas06@email.com>
2023-11-19 10:30:42 +0530

    Initial setup - Removed unwanted code - Added the required package

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Comments App</h1>
+      <p>CommentForm will go here</p>
+      <p>CommentList will go here</p>
+    </>
+  );
+}
+
+export default App;
