d0b55f4d041a2d845cec648200b0f6a74bd8d3a9
jayasuryas06@gmail.com <jayasuryas06@email.com>
2023-11-19 12:05:44 +0530

    Added Reply functionality to the Comment

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { CommonProps, PostProps } from "../types";
+import PostForm from "./PostForm";
+import { appStore } from "../stores/appStore";
 
 /* check if we can use discriminated unions to make this component more generic */
 // type CommentPostProps = {
@@ -21,7 +24,15 @@ type PostsProps = {
 };
 
 export default function Posts(props: PostsProps) {
+  const { addReply } = appStore();
+  const [currentFocusReplyId, setCurrentFocusReplyId] = useState("");
   const { type, data, postId } = props;
+
+  const handleOnAddReplySubmit = (e: CommonProps, id: string) => {
+    addReply(e, id);
+    setCurrentFocusReplyId("");
+  };
+
   return (
     <div className="px-4 py-2 max-w-[600px] m-auto flex flex-col border border-[#EFEFEF] rounded-sm">
       {data.map((post) => {
@@ -31,7 +42,22 @@ export default function Posts(props: PostsProps) {
               <div>{post.name}</div>
               <div>{post.message}</div>
               <div>{post.timestamp}</div>
+              {type === "comment" && (
+                <button
+                  onClick={() => setCurrentFocusReplyId(post.id)}
+                  className="text-blue-500 mr-3"
+                >
+                  Reply
+                </button>
+              )}
+              <button className="text-blue-500">Edit</button>
             </div>
+            {currentFocusReplyId === post.id && (
+              <PostForm
+                headerText="Reply"
+                onSubmit={(e) => handleOnAddReplySubmit(e, post.id)}
+              />
+            )}
             {post.replies && post.replies.length > 0 && (
               <Posts type="reply" data={post.replies} postId={post.id} />
             )}
